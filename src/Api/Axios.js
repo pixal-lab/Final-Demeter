e799f3fb43cf95c6c0de3fb3224c6d1b8f5c34bb
DevRaki <samuelriosaristizabal@gmail.com>
2023-12-14 16:11:09 -0500

    Update Axios.js

--- src/Api/Axios.js
+++ src/Api/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://https://demeter-final-back-production.up.railway.app',
+    baseURL: 'https://demeter-final-back-production.up.railway.app',
     withCredentials: true
 })
 
