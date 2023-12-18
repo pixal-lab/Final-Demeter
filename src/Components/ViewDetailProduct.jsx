441ff1c47ea53165bf3519270a88dbcdebee6e08
Pablo Ahumada <pablo.ahumada1@mail.udp.cl>
2023-12-17 21:19:31 -0300

    .

--- src/Components/ViewDetailProduct.jsx
+++ src/Components/ViewDetailProduct.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { useProduct } from '../Context/Product.context.jsx';
 import { AiFillDelete } from 'react-icons/ai';
 
 function ViewDetailProduct() {
 
-    const { getDetailProduct, detailP, deleteDetailProduct, CurrentProd } = useProduct();
 
     useEffect(() => {
         getDetailProduct(CurrentProd);
@@ -12,36 +10,6 @@ function ViewDetailProduct() {
 
     // const detailsArray = Array.isArray(detailP) ? detailP : [];
 
-    return (
-        <div className="table-responsive">
-            <table className="table table-hover">
-                <thead>
-                    <tr>
-                        <th className="text-center">Insumo</th>
-                        <th className="text-center">Cantidad</th>
-                        <th className="text-center">Acción</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {detailP.map((detail) => (
-                        <tr key={detail.ID_ProductDetail}>
-                            <td>{detail.Supply ? detail.Supply.Name_Supplies : ''}</td>
-                            <td>{detail.Lot_ProductDetail} {detail.Supply.measure}</td>
-                            <td>
-                                <div style={{ display: "flex", alignItems: "center", padding: '3px' }}>
-                                    <button
-                                        onClick={() => deleteDetailProduct(detail)}
-                                        className={`ml-1 btn btn-icon btn-danger`}
-                                    >
-                                        <AiFillDelete />
-                                    </button>
-                                </div>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    )
+    return 1
 }
 export default ViewDetailProduct
