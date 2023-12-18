2b4ee189d0c15a4817f685d5c00fb4cd8bf5aa95
Pablo Ahumada <pablo.ahumada1@mail.udp.cl>
2023-12-18 01:43:54 -0300

    .

--- src/Api/Axios.js
+++ src/Api/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://final-demeter-b-production.up.railway.app/',
+    baseURL: 'final-demeter-b-production.up.railway.app',
     withCredentials: true
 })
 // pixal estuvo aqui
